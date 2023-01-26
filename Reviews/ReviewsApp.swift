fae0a9f36523495d6abc5f4c526ee4e620438bbe
Amish Tufail <amish.tufail2002@gmail.com>
2023-01-26 15:43:59 +0500

    Initial Commit

---
+++ Reviews/ReviewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReviewsApp.swift
+//  Reviews
+//
+//  Created by Amish Tufail on 26/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct ReviewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
